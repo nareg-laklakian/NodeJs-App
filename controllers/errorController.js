d4e12dad88cb64b7e8ccde79e548df3177143a09
Laklak <nareglaklakian@gmail.com>
2022-08-17 00:13:03 +0400

    commit 4a

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,10 +1,51 @@
+const AppErrors = require('./../utils/appErrors');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppErrors(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational , trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    // Programming or other unknown error: 'we don't want to leak the error details to the client'
+  } else {
+    // 1) Log error
+    console.error('Error💥', err);
+    // 2) send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong here!!!',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   //   console.log(err.stack);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    sendErrorProd(error, res);
+  }
 };
+
+//  sendErrorProd is not working properly and it defaults to the else if error instead of the if isOperational error.
+/// not working the last thing in 118 fix the error message not showing
